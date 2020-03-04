7402002779ba89318c21696b6caa1977e4686c52
Vasyl Malanka <vsl.malanka@gmail.com>
2020-03-04 22:17:08 +0100

    Add configuration class that stores system configuration path

---
+++ src/app/code/Dex/PriceCalculation/Model/Configuration.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace Dex\PriceCalculation\Model;
+
+class Configuration
+{
+    public const XML_PATH_PRODUCT_PRICE_MULTIPLIER = 'dex/price_calculation/product_price_multiplier';
+}
