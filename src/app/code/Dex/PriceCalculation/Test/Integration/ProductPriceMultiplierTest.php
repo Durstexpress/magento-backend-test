2ca3c59af4db202fb5a50c2e80c567a1d0878fe9
Vasyl Malanka <vsl.malanka@gmail.com>
2020-03-07 10:47:06 +0100

    Add integration test for quote item price multiplication

---
+++ src/app/code/Dex/PriceCalculation/Test/Integration/ProductPriceMultiplierTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Dex\PriceCalculation\Test\Integration;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Quote\Model\Quote;
+use Magento\Quote\Model\Quote\Item;
+use PHPUnit\Framework\TestCase;
+use Magento\TestFramework\Helper\Bootstrap;
+
+class ProductPriceMultiplierTest extends TestCase
+{
+    /**
+     * @magentoAppIsolation enabled
+     * @magentoAppArea frontend
+     * @magentoDataFixture Magento/Catalog/_files/product_simple.php
+     * @magentoConfigFixture current_store dex/price_calculation/product_price_multiplier 4
+     * @return void
+     */
+    public function testProductPriceMultiplicationAfterAddingToCart(): void
+    {
+        $priceMultiplierValue = 4;
+
+        $objectManager = Bootstrap::getObjectManager();
+
+        $productRepository = $objectManager->create(ProductRepositoryInterface::class);
+        $product = $productRepository->get('simple');
+
+        $quoteItem = $objectManager->create(Item::class);
+        $quoteItem->setProduct($product);
+
+        $quote = $objectManager->create(Quote::class);
+        $quote->addItem($quoteItem);
+
+        $this->assertEquals($product->getPrice() * $priceMultiplierValue, $quoteItem->getCalculationPrice());
+    }
+}
